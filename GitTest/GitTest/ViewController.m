2b45fff312c39e998d6e621d65c751a71e326c78
mac <mac@macdemacbook-pro-83.local>
2016-06-30 16:25:52 +0800

    二次更新

--- GitTest/GitTest/ViewController.m
+++ GitTest/GitTest/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"许二");
+    NSLog(@"许老二,断半根");
 }
 
 - (void)didReceiveMemoryWarning {
